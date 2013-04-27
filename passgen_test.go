f8ddd0fdf02c874709a377b38aa9ffd1f63ab66d
Tijmen van der Burgt <tvdburgt@gmail.com>
2013-04-27 17:59:51 +0200

    Complete test file

--- passgen_test.go
+++ passgen_test.go
@@ -1,47 +1,50 @@
 package passgen
 
 import (
+	"bytes"
+	crand "crypto/rand"
+	"math/rand"
 	"testing"
 	"unicode"
 )
 
-func Benchmark128Complete(b *testing.B) {
+type randSource struct {
+	rand.Source
+}
+
+func (src *randSource) Read(p []byte) (int, error) {
+	for i := range p {
+		// Extract least significant byte from 63-bit integer
+		p[i] = byte(src.Int63() & 0xff)
+	}
+	return len(p), nil
+}
+
+func Benchmark256Hex(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Generate(128, Complete)
+		GenerateHex(256)
 	}
 }
 
-func Benchmark128WithoutSymbols(b *testing.B) {
+func Benchmark256Complete(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Generate(128, (Complete &^ Symbol))
+		Generate(256, SetComplete)
 	}
 }
 
-func Benchmark128Lower(b *testing.B) {
+func Benchmark256LowerDigit(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Generate(128, Lower)
+		Generate(256, SetLower|SetDigit)
 	}
 }
 
-func Benchmark32Complete(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		Generate(32, Complete)
-	}
-}
-
-func Benchmark32WithoutSymbols(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		Generate(32, (Complete &^ Symbol))
-	}
-}
-
-func TestRange(t *testing.T) {
-	pass, err := Generate(128, Complete)
+func TestCharRange(t *testing.T) {
+	pass, err := Generate(1000, SetComplete)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	var min, max byte = charOffset, charOffset + charRange
+	var min, max byte = 32, 126
 
 	for _, b := range pass {
 		if b < min || b > max {
@@ -51,16 +54,71 @@ func TestRange(t *testing.T) {
 	}
 }
 
-func TestWithoutSymbols(t *testing.T) {
-	flags := Complete &^ Symbol
-	pass, err := Generate(128, flags)
+func TestLen(t *testing.T) {
+	n := 32
+	pass, err := Generate(n, SetComplete)
 	if err != nil {
 		t.Fatal(err)
 	}
 
+	if len(pass) != n {
+		t.Errorf("len(pass) = %d, expected %d",
+			len(pass), n)
+	}
+
+	pass, err = Generate(0, SetComplete)
+	if err != ErrLength {
+		t.Fatalf("Expected ErrLength error")
+	}
+}
+
+func TestGenerateHex(t *testing.T) {
+	Reader = &randSource{rand.NewSource(0)}
+
+	expected := []byte("01c073624aaf3978514ef8443bb2a859c75fc3cc6af26d5aaa20926f046baa66")
+	pass, err := GenerateHex(len(expected))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if bytes.Compare(pass, expected) != 0 {
+		t.Errorf(`Passwords don't match:
+	Expected:  %s
+	Generated: %s`, expected, pass)
+	}
+
+	Reader = crand.Reader
+}
+
+func TestGenerate(t *testing.T) {
+	Reader = &randSource{rand.NewSource(0)}
+
+	expected := []byte("a5XL<C8ONID>}SV12T$q.3c1$Z-_]8HrGTpU.iDRw'?0`^0B]P9y>7TMA[FO\"jbe")
+
+	pass, err := Generate(len(expected), SetComplete)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if bytes.Compare(pass, expected) != 0 {
+		t.Errorf(`Passwords don't match:
+	Expected:  %s
+	Generated: %s`, expected, pass)
+	}
+
+	Reader = crand.Reader
+}
+
+func TestWithoutSymbols(t *testing.T) {
+	mask := SetComplete &^ SetSymbol
+	pass, err := Generate(1000, mask)
+	if err != nil {
+		t.Fatal(err)
+	}
 	for _, b := range pass {
 		if unicode.IsSymbol(rune(b)) {
-			t.Errorf("Encountered symbol '%c' with flag %b", b, flags)
+			t.Errorf("Encountered symbol '%c' with mask %b",
+				b, mask)
 		}
 	}
 }
